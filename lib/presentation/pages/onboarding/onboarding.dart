152336d45dbba83e75f269a28a4fcb240736c3b1
yahia samir <yahiasamir70@gmail.com>
2025-03-14 17:11:36 +0200

    On Boarding screen finish

--- lib/presentation/pages/onboarding/onboarding.dart
+++ lib/presentation/pages/onboarding/onboarding.dart
@@ -1,4 +1,5 @@
 import 'package:fixiez/core/constants/image_assets.dart';
+import 'package:fixiez/core/routes/app_routes.dart';
 import 'package:fixiez/core/theme/app_colors.dart';
 import 'package:fixiez/core/theme/app_text.dart';
 import 'package:fixiez/presentation/widgets/cutom_button.dart';
@@ -17,22 +18,45 @@ class OnBoarding extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.center,
         textDirection: TextDirection.rtl,
         children: [
-        SvgPicture.asset(ImageAssets.onBoarding,width: 328.w,height: 328.h,fit: BoxFit.cover,),
-        SizedBox(height: 25.h,),
-        SizedBox(width: 375.w,
-        height: 65.h,
-          child: Center(child: Text('أسهل طريقة لتنفيذ طلبك',style: context.bold28Blue,))),
-        Padding(
-          padding:  EdgeInsets.symmetric(horizontal: 32.w,),
-          child: Text('الآن يمكنك العثور على أمهر الصنايعية في منطقتك بكل سهولة اطلب خدمتك ، واستمتع بخدمة سريعة وموثوقة وأمان تام',style: context.reg16Hint80,),
-        ),
-        SizedBox(height: 38.h,),
-        CustomButton(text: 'تسجيل الدخول', onpressed: (){},),
-        SizedBox(height: 24.h,),
-        CustomButton(text: 'انشاء حساب', onpressed: (){},backgroundColor: AppColors.white,foregroundColor: AppColors.primary,),
-
-
-      ]),
+          SvgPicture.asset(
+            ImageAssets.onBoarding,
+            width: 328.w,
+            height: 328.h,
+            fit: BoxFit.cover,
+          ),
+          SizedBox(height: 25.h),
+          SizedBox(
+            width: 375.w,
+            height: 65.h,
+            child: Center(
+              child: Text('أسهل طريقة لتنفيذ طلبك', style: context.bold28Blue),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 32.w),
+            child: Text(
+              'الآن يمكنك العثور على أمهر الصنايعية في منطقتك بكل سهولة اطلب خدمتك ، واستمتع بخدمة سريعة وموثوقة وأمان تام',
+              style: context.reg16Hint80,
+            ),
+          ),
+          SizedBox(height: 38.h),
+          CustomButton(
+            text: 'تسجيل الدخول',
+            onpressed: () {
+              Navigator.pushNamed(context, AppRoutes.login);
+            },
+          ),
+          SizedBox(height: 24.h),
+          CustomButton(
+            text: 'انشاء حساب',
+            onpressed: () {
+              Navigator.pushNamed(context, AppRoutes.signup);
+            },
+            backgroundColor: AppColors.white,
+            foregroundColor: AppColors.primary,
+          ),
+        ],
+      ),
     );
   }
 }
