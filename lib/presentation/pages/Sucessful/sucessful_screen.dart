4287007df1748f520f94d10a9ef19b90341a0ba8
yahia samir <yahiasamir70@gmail.com>
2025-03-16 17:36:14 +0200

    rename Successful screen

--- lib/presentation/pages/Sucessful/sucessful_screen.dart
+++ lib/presentation/pages/Sucessful/sucessful_screen.dart
@@ -6,8 +6,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class Sucessful extends StatelessWidget {
-  const Sucessful({super.key});
+class Successful extends StatelessWidget {
+  const Successful({super.key});
 
   @override
   Widget build(BuildContext context) {
