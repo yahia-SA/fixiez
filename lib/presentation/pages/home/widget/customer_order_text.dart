b187946fdd24273b04e3329d3202cf81b5e540d7
yahia samir <yahiasamir70@gmail.com>
2025-03-17 02:20:28 +0200

    add screen util to widget

--- lib/presentation/pages/home/widget/customer_order_text.dart
+++ lib/presentation/pages/home/widget/customer_order_text.dart
@@ -1,6 +1,7 @@
 import 'package:fixiez/core/theme/app_colors.dart';
 import 'package:fixiez/core/theme/app_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CustomerOrderText extends StatelessWidget {
   const CustomerOrderText({super.key});
@@ -9,22 +10,22 @@ class CustomerOrderText extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Container(
-        width: 326,
-        height: 100,
+        width: 326.w,
+        height: 100.h,
         decoration: BoxDecoration(
           color: AppColors.white,
-          borderRadius: BorderRadius.circular(8),
-          border: Border.all(color: Colors.grey, width: 0.5),
+          borderRadius: BorderRadius.circular(8.r),
+          border: Border.all(color: Colors.grey, width: 0.5.w),
         ),
         child: ClipRRect(
-          borderRadius: BorderRadius.circular(8),
+          borderRadius: BorderRadius.circular(8.r),
           child: TextField(
             expands: true,
             maxLines: null,
             minLines: null,
             textAlign: TextAlign.center,
             textAlignVertical: TextAlignVertical.center,
-            style: AppText.reg16.copyWith(color: const Color(0xff808080)),
+            style: context.reg16Hint80,
             decoration: const InputDecoration(
               hintText: 'اوصف العمل المطلوب',
               border: InputBorder.none,
