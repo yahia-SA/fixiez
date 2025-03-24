cb1d370a28155863b8109664483460868fad6411
amrmahmoud333 <3mrma7moud69@gmail.com>
2025-03-25 00:55:39 +0200

    edit states

--- lib/presentation/state/cubit/repair_state.dart
+++ lib/presentation/state/cubit/repair_state.dart
@@ -1,19 +1,75 @@
 part of 'repair_cubit.dart';
 
-sealed class RepairState extends Equatable {
-  const RepairState();
+class RepairFormData extends Equatable {
+  const RepairFormData({
+    this.location,
+    this.unitNumber,
+    this.description,
+    this.serviceName,
+    this.serviceType,
+    this.date,
+  });
+  final String? location;
+  final String? unitNumber;
+  final String? description;
+  final ServiceName? serviceName;
+  final ServiceType? serviceType;
+  final String? date;
+
+  RepairFormData copyWith({
+    String? location,
+    String? unitNumber,
+    String? description,
+    ServiceName? serviceName,
+    ServiceType? serviceType,
+    String? date,
+  }) {
+    return RepairFormData(
+      location: location ?? this.location,
+      unitNumber: unitNumber ?? this.unitNumber,
+      description: description ?? this.description,
+      serviceName: serviceName ?? this.serviceName,
+      serviceType: serviceType ?? this.serviceType,
+      date: date ?? this.date,
+    );
+  }
 
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [
+    location,
+    unitNumber,
+    description,
+    serviceName,
+    serviceType,
+    date,
+  ];
 }
 
-final class RepairInitial extends RepairState {}
-final class RepairLoading extends RepairState {}
-final class Repairsuccess extends RepairState {}
-final class RepairFailure extends RepairState {
-  const RepairFailure(this.message);
-  final String message;
-    @override
-  List<Object> get props => [message];
+// 2. Fix state classes
+sealed class RepairState extends Equatable {
+  const RepairState(this.formData);
+  final RepairFormData formData;
 
+  @override
+  List<Object> get props => [formData];
+}
+
+final class RepairInitial extends RepairState {
+  const RepairInitial(super.formData);
+}
+
+final class RepairLoading extends RepairState {
+  const RepairLoading(super.formData);
+}
+
+final class RepairSuccess extends RepairState {
+  const RepairSuccess(super.formData);
+}
+
+final class RepairFailure extends RepairState {
+  const RepairFailure(super.formData, this.message);
+  final String message;
+
+  @override
+  List<Object> get props => [message, ...super.props];
 }
