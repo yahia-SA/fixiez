3308c17b6941cb897a4d7dc6928d4c410180a8fd
amrmahmoud333 <3mrma7moud69@gmail.com>
2025-03-26 00:33:50 +0200

    add review models

---
+++ lib/data/models/reviews_model.dart
@@ -0,0 +1,151 @@
+class ReviewsModel {
+  ReviewsModel.fromJson(Map<String, dynamic> json) {
+    status = json['status'];
+    message = json['message'];
+    if (json['data'] != null) {
+      data = <Data>[];
+      json['data'].forEach((v) {
+        data!.add(Data.fromJson(v));
+      });
+    }
+    metadata =
+        json['metadata'] != null ? Metadata.fromJson(json['metadata']) : null;
+  }
+  ReviewsModel({this.status, this.message, this.data, this.metadata});
+  String? status;
+  String? message;
+  List<Data>? data;
+  Metadata? metadata;
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['status'] = status;
+    data['message'] = message;
+    if (this.data != null) {
+      data['data'] = this.data!.map((v) => v.toJson()).toList();
+    }
+    if (metadata != null) {
+      data['metadata'] = metadata!.toJson();
+    }
+    return data;
+  }
+}
+
+class Data {
+  Data({this.sId, this.userId, this.comment, this.createdAt, this.updatedAt});
+
+  Data.fromJson(Map<String, dynamic> json) {
+    sId = json['_id'];
+    userId = json['userId'] != null ? UserId.fromJson(json['userId']) : null;
+    comment = json['comment'];
+    createdAt = json['createdAt'];
+    updatedAt = json['updatedAt'];
+  }
+  String? sId;
+  UserId? userId;
+  String? comment;
+  String? createdAt;
+  String? updatedAt;
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['_id'] = sId;
+    if (userId != null) {
+      data['userId'] = userId!.toJson();
+    }
+    data['comment'] = comment;
+    data['createdAt'] = createdAt;
+    data['updatedAt'] = updatedAt;
+    return data;
+  }
+}
+
+class UserId {
+  UserId({
+    this.sId,
+    this.name,
+    this.password,
+    this.phoneNumber,
+    this.role,
+    this.isActive,
+    this.balance,
+    this.cashBack,
+    this.createdAt,
+    this.updatedAt,
+    this.iV,
+  });
+
+  UserId.fromJson(Map<String, dynamic> json) {
+    sId = json['_id'];
+    name = json['name'];
+    password = json['password'];
+    phoneNumber = json['phoneNumber'];
+    role = json['role'];
+    isActive = json['isActive'];
+    balance = json['balance'];
+    cashBack = json['cashBack'];
+    createdAt = json['createdAt'];
+    updatedAt = json['updatedAt'];
+    iV = json['__v'];
+  }
+  String? sId;
+  String? name;
+  String? password;
+  String? phoneNumber;
+  String? role;
+  bool? isActive;
+  int? balance;
+  int? cashBack;
+  String? createdAt;
+  String? updatedAt;
+  int? iV;
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['_id'] = sId;
+    data['name'] = name;
+    data['password'] = password;
+    data['phoneNumber'] = phoneNumber;
+    data['role'] = role;
+    data['isActive'] = isActive;
+    data['balance'] = balance;
+    data['cashBack'] = cashBack;
+    data['createdAt'] = createdAt;
+    data['updatedAt'] = updatedAt;
+    data['__v'] = iV;
+    return data;
+  }
+}
+
+class Metadata {
+  Metadata({
+    this.totalItems,
+    this.totalPages,
+    this.skip,
+    this.pageIndex,
+    this.pageSize,
+  });
+
+  Metadata.fromJson(Map<String, dynamic> json) {
+    totalItems = json['totalItems'];
+    totalPages = json['totalPages'];
+    skip = json['skip'];
+    pageIndex = json['pageIndex'];
+    pageSize = json['pageSize'];
+  }
+  int? totalItems;
+  int? totalPages;
+  int? skip;
+  int? pageIndex;
+  int? pageSize;
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['totalItems'] = totalItems;
+    data['totalPages'] = totalPages;
+    data['skip'] = skip;
+    data['pageIndex'] = pageIndex;
+    data['pageSize'] = pageSize;
+    return data;
+  }
+}
