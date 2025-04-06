633c4abacc6895bf6f4ba72d6da926caf60398e7
yahia samir <yahiasamir70@gmail.com>
2025-04-06 17:38:09 +0200

    add enum for service name arabic

--- lib/core/constants/enums.dart
+++ lib/core/constants/enums.dart
@@ -5,3 +5,36 @@ enum ServiceName { plumbing, carpentry, electricity, painting }
 enum ServiceType { urgent_request, normal_request }
 
 enum OtpPages {signup,forgetPassword}
+
+extension ServiceNameE on ServiceName {
+  String get arabicName {
+    switch (this) {
+      case ServiceName.plumbing:
+        return 'سباكة';
+      case ServiceName.carpentry:
+        return 'نجارة';
+      case ServiceName.electricity:
+        return 'كهرباء';
+      case ServiceName.painting:
+        return 'دهانات';
+    }
+  }
+}
+ServiceName? serviceNameFromString(String name) {
+  return ServiceName.values.firstWhere(
+    (e) => e.name == name,
+    orElse: () => ServiceName.plumbing,
+  );
+}
+extension UserTypeArabicExtension on String {
+  String get toArabicRole {
+    switch (this) {
+      case 'user':
+        return 'مستخدم';
+      case 'admin':
+        return 'ادمن';
+      default:
+        return this;
+    }
+  }
+}
